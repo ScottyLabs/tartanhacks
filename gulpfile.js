bd9957580f5e88c7e6b56f3cdfa2f0a370d1a864
Oscar Bezi <oscar@bezi.io>
2014-11-23 11:56:16 -0500

    Skeleton file layout added.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * @file gulpfile.js
+ * @brief Bootstraps the gulpfile.coffee file.
+ * @author Oscar Bezi, oscar@bezi.io
+ * @since 23 November 2014
+ */
+
+require('coffee-script/register')
+require('./gulpfile.coffee')
