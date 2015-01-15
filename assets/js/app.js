96437ff116e17e778c7d41579d79a7323e181eed
Oscar Bezi <oscar@bezi.io>
2015-01-14 22:20:46 -0500

    Fixed files.

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+particlesJS('splash-bg', {
+  particles: {
+    color: '#fff',
+    shape: 'triangle',
+    opacity: 1,
+    size: 2,
+    size_random: false,
+    nb: $(window).width() / 5,
+    line_linked: {
+      enable_auto: true,
+      distance: 100,
+      color: '#fff',
+      opacity: 0.9,
+      widapp: 1,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    enable: false
+  },
+  retina_detect: true
+});
