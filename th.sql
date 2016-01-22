694f30b91d0a81f4219199d329f05bf8fd8ac2f8
Oscar Bezi <oscar@bezi.io>
2016-01-22 04:44:22 -0500

    Updated .sql file.

--- th.sql
+++ th.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 5.6.27, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Linux (x86_64)
 --
--- Host: localhost    Database: th_2016
+-- Host: unix.bezi.io    Database: th_2016
 -- ------------------------------------------------------
 -- Server version	5.6.27-0ubuntu0.14.04.1
 
@@ -27,6 +27,20 @@ CREATE TABLE `announcements` (
   `text` varchar(500) NOT NULL,
   `timestamp` varchar(100) NOT NULL,
   PRIMARY KEY (`announcement_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_statuses`
+--
+
+DROP TABLE IF EXISTS `user_statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,9 +63,18 @@ CREATE TABLE `users` (
   `student_class` varchar(100) DEFAULT NULL,
   `personal_url` varchar(100) DEFAULT NULL,
   `in_resume_drop` tinyint(1) DEFAULT NULL,
-  `is_admin` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `is_registered` tinyint(1) DEFAULT NULL,
+  `is_priority_registration` tinyint(1) DEFAULT NULL,
+  `registration_timestamp` varchar(100) DEFAULT NULL,
+  `user_status` int(11) NOT NULL DEFAULT '1',
+  `github` varchar(100) DEFAULT NULL,
+  `linkedin` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `google_id` (`google_id`),
+  UNIQUE KEY `andrewID` (`andrewID`),
+  KEY `user_status` (`user_status`),
+  CONSTRAINT `user_status` FOREIGN KEY (`user_status`) REFERENCES `user_statuses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -63,4 +86,55 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-16 19:11:56
+-- Dump completed on 2016-01-22  4:43:40
+-- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Linux (x86_64)
+--
+-- Host: unix.bezi.io    Database: th_2016
+-- ------------------------------------------------------
+-- Server version	5.6.27-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_statuses`
+--
+
+DROP TABLE IF EXISTS `user_statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_statuses`
+--
+
+LOCK TABLES `user_statuses` WRITE;
+/*!40000 ALTER TABLE `user_statuses` DISABLE KEYS */;
+INSERT INTO `user_statuses` VALUES (1,'UNREGISTERED'),(2,'ADMIN'),(4,'MENTOR'),(5,'HACKER_CHECKED_IN'),(6,'HACKER_ACCEPTED'),(7,'HACKER_WAITLISTED');
+/*!40000 ALTER TABLE `user_statuses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-22  4:44:08
