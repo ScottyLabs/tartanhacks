3149f995448e91209eff15e4b1744e16494a8d5e
Scott Krulcik <skrulcik@gmail.com>
2016-12-01 21:45:24 -0500

    Inital 2017 commit

---
+++ js/spawn-accents.js
@@ -0,0 +1,16 @@
+
+var accentContainer = $("#accent-container");
+var containerHeight = accentContainer.height;
+var accentHeight = '2em';
+var accentWidths = [
+    '2em',
+    '8em', '8em',
+    '12em', '12em', '12em', '12em',
+]
+
+var randomElement = function(array) {
+    return array[Math.random() * array.length];
+}
+
+
+
--- static/src/js/Dispatcher.js
+++
@@ -1,8 +0,0 @@
-/* @file Dispatcher.js
- * @brief Creates a dispatcher.
- *
- * @author Oscar Bezi (bezi@scottylabs)
- */
-'use strict';
-var Dispatcher = require('flux').Dispatcher;
-module.exports = new Dispatcher();
