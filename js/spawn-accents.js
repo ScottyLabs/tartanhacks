a8d68ba4ba6f78a96d0e47cd232bdf4253df88e2
Scott Krulcik <skrulcik@gmail.com>
2016-12-02 16:08:32 -0500

    Simplify accent spawning, more JS cleanup
    - Removed overly complicated two phase animation
    - Treat accent container as a single object (id instead of class)
    - Organized JS constants

--- js/spawn-accents.js
+++ js/spawn-accents.js
@@ -1,16 +1,30 @@
-var accentContainer = $('.accent-container')[0];
-var containerHeight = $(accentContainer).height();
-var containerWidth = $(accentContainer).width();
-var accents = $(".accent");
-var accentHeight = 32;
+// spawn-accents.js
+//
+// Creates accent animations for the TartanHacks 2017 landing page. "Accents"
+// refer to the rectangular colored blocks used for TartanHacks 2017 branding.
+// Once spawned, accents will scroll across the screen in a continuous loop.
+// The appearance of
+//
+// Accent colors and concept by Noah Johnson, implementation by Scott Krulcik.
+// (c) 2017 ScottyLabs
 
+// Accent Container - Parent object to hold all accents
+var ACCENT_CONTAINER = $('div#accent-container');
+console.assert(ACCENT_CONTAINER,
+    "Accent container cannot be found. Cannot spawn accents.");
+var CONTAINER_HEIGHT = $(ACCENT_CONTAINER).height();
+var CONTAINER_WIDTH = $(ACCENT_CONTAINER).width();
+
+// Animation Options
+// Minimum rate of 40 px/s, maximum of 200 px/s
+var MIN_DURATION = Math.floor(CONTAINER_WIDTH / 200);
+var MAX_DURATION = Math.floor(CONTAINER_WIDTH / 40);
+var MAX_DELAY = Math.floor((MAX_DURATION - MIN_DURATION) / 2);
+
+// Accent Styling (see accent.css)
+var ACCENT_HEIGHT = 32;
 var NUM_ACCENT_COLORS = 5;
 var NUM_ACCENT_WIDTHS = 3;
-var MAX_DELAY = 2;
-// Minimum rate of 200 px/s, maximum of 40 px/s
-var MIN_DURATION = Math.floor(containerWidth / 200);
-var MAX_DURATION = Math.floor(containerWidth / 40);
-
 
 // Generate a random integer in the range [start, end)
 var randInt = function(start, end) {
@@ -29,11 +43,10 @@ var createProperty = function(value, units) {
     return '' + value + units;
 };
 
-// Generates a random delay between
+// Generate animation parameters
 var generateDuration = function() {
     return createProperty(randDecimal(MIN_DURATION, MAX_DURATION), 's');
 };
-
 var generateDelay = function() {
     return createProperty(randDecimal(0, MAX_DELAY), 's');
 };
@@ -55,52 +68,38 @@ var generateAccent = function() {
     return accent;
 };
 
-
 // Fills the screen with n evenly spaced accents, starting from the base offset
-var fillAccentContainer = function(n, spacing, baseOffset) {
+var fillAccentContainer = function(n, laneHeight) {
     console.assert(n > 0, '"n" must be a positive integer');
 
     // The amount of vertical space between accents. Must be more than the
     // height of a single accent to ensure they do not overlap
-    if (spacing < accentHeight) {
-        console.log("adjusting spacing");
-        spacing = accentHeight;
+    if (laneHeight < ACCENT_HEIGHT) {
+        console.warn('Cannot fit accents in lanes. Increasing height from '
+            + laneHeight + ' to ' + ACCENT_HEIGHT);
+        laneHeight = ACCENT_HEIGHT;
     }
     // If the given number of accents will not fit on screen, add fewer of them
     // than requested. This is the web, so doing unexpected things to
     // compensate for odd screen sizes is sort of the norm.
-    if (spacing * n > containerHeight) {
-        console.log("adjusting spacing");
-        var msg = 'Cannot fit all accents on page. Reducing count from ' + n;
-        n = Math.floor(containerHeight / n);
-        msg += ' to ' + n;
-        console.warn(msg);
+    if (laneHeight * n > CONTAINER_HEIGHT) {
+        var newN = Math.floor(CONTAINER_HEIGHT / n);
+        console.warn('Cannot fit all accents on page. Reducing count from '
+            + n + ' to ' + newN);
+        n = newN;
     }
     // Generate randomly styled accents and place them at their appropriate
     // offset positions
     for (var i = 0; i < n; i++) {
-        console.log(baseOffset + i * spacing);
         generateAccent()
-            .css({ 'top': createProperty(baseOffset + i * spacing, 'px') })
-            .appendTo(accentContainer);
+            .css({ 'top': createProperty(i * laneHeight, 'px') })
+            .appendTo(ACCENT_CONTAINER);
     }
 };
 
 // The amount of space each accent should have in the final animation.
-var laneSize = accentHeight * 1.5;
-var totalLanes = Math.floor(containerHeight / laneSize);
+var laneSize = ACCENT_HEIGHT * 1.5;
+var totalLanes = Math.floor(CONTAINER_HEIGHT / laneSize);
 
-// Two stage animation sequence that will generate half of the accents after
-// a delay to ensure that the accents don't all clump up. The first half of the
-// accents will fill lanes 0,2,4... and the second half will take lanes
-// 1,3,5...
-fillAccentContainer(Math.floor(totalLanes / 2), laneSize * 2, 0);
-// Delaying the second round until about half of the first round is finished
-// ensures we have a slow start, instead of just overwhelming the screen with
-// accents. The same effect would be achieved doing all accents at once with
-// longer delays, but the resulting output is more likely to have clumps
-var reasonableDelay = ((MAX_DELAY + MAX_DURATION - MIN_DURATION) / 2) * 1000;
-setTimeout(function() {
-    fillAccentContainer(Math.floor(totalLanes / 2), laneSize * 2, laneSize);
-}, reasonableDelay);
+fillAccentContainer(totalLanes, laneSize);
 
