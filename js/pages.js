ScottyLabs/tartanhacks

commit 4485edeb62365a6298384a5c8f9b8341cd19be81

Fix FAQ Bug
Page buttons were being hidden, but the page button wrapper was still
blocking the last FAQ on the page.

js/pages.js
@@ -8,7 +8,7 @@
 // The page map holds selectors for the elements that should be hidden or shown
 // for the desired page
 var pageMap = {
-    "main": ["#accent-frame", ".static-center.container", ".page-button"],
+    "main": ["#accent-frame", ".static-center.container", ".page-button-wrapper"],
     "faq": ["#faq", ".logo.accent-logo", ".back-button", ".modal-logo"],
     "schedule": ["#schedule", ".logo.accent-logo", ".back-button", ".modal-logo"]
 };

commit 1cac2a6c3e6c0ef19058b3960be54a0508fec2e0

Fix animation bug caused by 4485ede

js/pages.js
@@ -8,7 +8,7 @@
 // The page map holds selectors for the elements that should be hidden or shown
 // for the desired page
 var pageMap = {
-    "main": ["#accent-frame", ".static-center.container", ".page-button-wrapper"],
+    "main": ["#accent-frame", ".static-center.container", ".page-button-wrapper", ".page-button"],
     "faq": ["#faq", ".logo.accent-logo", ".back-button", ".modal-logo"],
     "schedule": ["#schedule", ".logo.accent-logo", ".back-button", ".modal-logo"]
 };