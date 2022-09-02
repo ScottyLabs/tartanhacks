c7edd54e3f476d333b4a8a92223041b5ba434fa1
Gram Liu <gram.liu226@gmail.com>
2022-09-02 17:56:39 -0400

    Add baseline mountain background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from "next";
 import Text from "../src/components/Text";
-import TartanHacksIcon from "../src/icons/TartanHacksIcon";
+import TartanHacksIcon from "../src/svg/TartanHacksIcon";
 import BaseLayout from "../src/layouts/BaseLayout";
 import styles from "../styles/Home.module.scss";
+import HeroBackground from "../src/svg/HeroBackground";
 
 const Home: NextPage = () => {
   return (
     <BaseLayout className={styles.main}>
       <TartanHacksIcon className={styles.homeIcon} />
+      <div className={styles.heroBackground}>
+        <HeroBackground />
+      </div>
       <div className={styles.heroText}>
         <div className={styles.heroSpacerStart} />
         <Text variant="hero" className={styles.heroTitle}>
