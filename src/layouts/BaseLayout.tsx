e9f92e735be5892e5802a124d754509afb138f4a
David Ethan Hwang <hdavidethan@gmail.com>
2023-01-02 12:16:00 +0800

    Update meta tags for TartanHacks 2023

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -15,28 +15,31 @@ export default function BaseLayout({ children, className }: Props) {
   return (
     <>
       <Head>
-        <title>TartanHacks</title>
-        <meta name="title" content="TartanHacks" />
+        <title>TartanHacks 2023 | Feb 3-4, 2023</title>
+        <meta name="title" content="TartanHacks 2023 | Feb 3-4, 2023" />
         <meta
           name="description"
-          content="ScottyLabs a community of passionate, interdisciplinary leaders that use design and technology to achieve more."
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
         />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://tartanhacks.org/" />
-        <meta property="og:title" content="TartanHacks" />
+        <meta property="og:url" content="https://tartanhacks.com/" />
+        <meta property="og:title" content="TartanHacks 2023 | Feb 3-4, 2023" />
         <meta
           property="og:description"
-          content="ScottyLabs a community of passionate, interdisciplinary leaders that use design and technology to achieve more."
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
         />
         <meta property="og:image" content="/cover-photo.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://tartanhacks.org/" />
-        <meta property="twitter:title" content="TartanHacks" />
+        <meta property="twitter:url" content="https://tartanhacks.com/" />
+        <meta
+          property="twitter:title"
+          content="TartanHacks 2023 | Feb 3-4, 2023"
+        />
         <meta
           property="twitter:description"
-          content="ScottyLabs a community of passionate, interdisciplinary leaders that use design and technology to achieve more."
+          content="TartanHacks is Carnegie Mellon's largest hackathon! It is a 24-hour hackathon where participants from all over the country create innovative projects."
         />
         <meta property="twitter:image" content="/cover-photo.png" />
         <link rel="icon" href="/favicon.png" />
