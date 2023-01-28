4571fd55e13ebe78ba7bf4047820fcc94db40cbf
Bradley Teo <bradleyteowj@gmail.com>
2023-01-28 15:44:44 -0500

    Add schedule

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,8 +1,57 @@
-function Block({ title, time, className }: { title: string, time: string, className: string }) {
+import styles from './Schedule.module.css';
+
+function TopBlock({
+                    title,
+                    time,
+                    area,
+                    location,
+                  }: {
+  title: string;
+  time: string;
+  area: string;
+  location?: string;
+}) {
   return (
-    <div className={`${className} border p-2 rounded border-gray-600 flex flex-col justify-center`}>
+    <div
+      className={`border p-1 rounded border-blue text-gray-200 flex flex-row justify-center items-center`}
+      style={{ gridArea: area }}
+    >
       <h1 className='text-lg text-center'>{title}</h1>
-      <p className='text-sm text-center text-gray-400'>{time}</p>
+      <p className='text-sm text-center text-gray-400 ml-4 flex flex-row'>
+        <span>{time}</span>
+        {location && (
+          <>
+            <span className='mx-2'>⸱</span>
+            <span>{location}</span>
+          </>
+        )}
+      </p>
+    </div>
+  );
+}
+
+function Block({
+                 title,
+                 time,
+                 area,
+                 location,
+               }: {
+  title: string;
+  time: string;
+  area: string;
+  location: string;
+}) {
+  return (
+    <div
+      className={`border p-2 rounded border-gray-600 flex flex-col justify-center`}
+      style={{ gridArea: area }}
+    >
+      <h1 className='text-lg text-center'>{title}</h1>
+      <p className='text-sm text-center text-gray-400 flex flex-row justify-center'>
+        <span>{time}</span>
+        <span className='mx-2'>⸱</span>
+        <span>{location}</span>
+      </p>
     </div>
   );
 }
@@ -14,31 +63,112 @@ export default function Schedule() {
         <div className='text-beige m-auto'>
           <h3 className='text-2xl mb-4 text-center text-yellow'>Schedule</h3>
           <div className='max-w-4xl m-auto'>
-            { /* For more flexibility, can use grid-template-areas */}
-            <div className='grid grid-cols-4 gap-x-2 gap-y-2'>
-              <div className='col-span-2 col-start-1 row-span-1 text-center text-xl text-yellow'>Feb 3</div>
-              <div className='col-span-2 col-start-3 row-span-1 text-center text-xl text-yellow'>Feb 4</div>
-              <Block time='10am - 11am' title='Breakfast' className='col-span-2 col-start-3 row-span-1' />
-              <Block time='1pm - 2pm' title='Lunch' className='col-span-2 col-start-3 row-span-1' />
-              <Block time='2pm - 3pm' title='Mike & Nehal Q&A' className='col-span-2 col-start-3 row-span-1' />
-              <Block time='4:30pm - 5:30pm' title='Opening Ceremony'
-                     className='col-span-2 col-start-1 row-span-1 row-start-5' />
-              <Block time='6pm' title='Hacking Starts'
-                     className='col-span-2 col-start-1 row-span-1 row-start-6' />
-              <Block time='6pm' title='Hacking Ends'
-                     className='col-span-2 col-start-3 row-span-1 row-start-6' />
-              <Block time='6pm - 7pm' title='Mixer'
-                     className='col-span-2 col-start-1 row-span-1 row-start-7' />
-              <Block time='6pm - 7pm' title='Dinner'
-                     className='col-span-2 col-start-3 row-span-1 row-start-7' />
-              <Block time='7pm - 8pm' title='RoboClub Talk'
-                     className='col-span-1 col-start-1 row-span-1 row-start-8' />
-              <Block time='7pm - 8pm' title='Algorand Talk'
-                     className='col-span-1 col-start-2 row-span-1 row-start-8' />
-              <Block time='8pm - 9pm' title='Dinner'
-                     className='col-span-2 col-start-1 row-span-1 row-start-8' />
-              <Block time='8:30pm - 9:30pm' title='Closing Ceremony'
-                     className='col-span-2 col-start-3 row-span-1 row-start-8' />
+            {/* For more flexibility, can use grid-template-areas */}
+            <div className={`grid gap-x-2 gap-y-2 ${styles.grid}`}>
+              <div
+                className='text-center text-xl text-yellow mt-3'
+                style={{ gridArea: 'feb3' }}
+              >
+                Feb 3
+              </div>
+              <div
+                className='text-center text-xl text-yellow mt-3'
+                style={{ gridArea: 'feb4' }}
+              >
+                Feb 4
+              </div>
+              <Block
+                area='c10am'
+                title='Breakfast'
+                time='10am - 11am'
+                location='Rangos Ballroom'
+              />
+              <Block
+                area='c12pm'
+                title='Talk with Jasmine Lawrence'
+                time='12pm - 1pm'
+                location='Virtual'
+              />
+              <Block
+                area='c1pm'
+                title='Lunch'
+                time='1pm - 2pm'
+                location='Rangos Ballroom'
+              />
+              <Block
+                area='c2pm'
+                title='Q&A with Mike and Nehal'
+                time='2pm - 3pm'
+                location='Virtual'
+              />
+              <TopBlock
+                area='a3pm'
+                title='Check In Starts'
+                time='3pm'
+                location='Connan Room'
+              />
+              <Block
+                area='a430pm'
+                title='Opening Ceremony'
+                time='4:30pm - 5:30pm'
+                location='McConomy Auditorium'
+              />
+              <TopBlock area='a6pm' title='Hacking Begins' time='6pm' />
+              <TopBlock area='c6pm' title='Hacking Ends' time='6pm' />
+              <Block
+                area='a6pm2'
+                title='Team Formation Mixer'
+                time='6pm - 7pm'
+                location='Connan Room'
+              />
+              <Block
+                area='c6pm2'
+                title='Dinner'
+                time='6pm - 7pm'
+                location='Rangos Ballroom'
+              />
+              <Block
+                area='a7pm'
+                title='RoboClub Talk'
+                time='7pm - 8pm'
+                location='Dowd'
+              />
+              <Block
+                area='b7pm'
+                title='Algorand Talk'
+                time='7pm - 8pm'
+                location='Connan'
+              />
+              <Block
+                area='c7pm'
+                title='Judging'
+                time='7pm - 8pm'
+                location='Rangos Ballroom'
+              />
+              <Block
+                area='a8pm'
+                title='Dinner'
+                time='8pm - 9pm'
+                location='Rangos Ballroom'
+              />
+              <Block
+                area='c830pm'
+                title='Closing Ceremony'
+                time='8:30pm - 9:30pm'
+                location='McConomy Auditorium'
+              />
+              <Block
+                area='a9pm'
+                title='Hacker Hangout'
+                time='9pm - 11pm'
+                location='Connan Room'
+              />
+              <Block
+                area='a1130pm'
+                title='Karaoke & Midnight Snack'
+                time='11:30pm - 2:00am'
+                location='Connan Room'
+              />
             </div>
           </div>
         </div>
