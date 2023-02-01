fce414da0d70fc5a0485f6ce7e44beea54a80263
Anuda Weerasinghe <anuda@live.com>
2023-02-01 11:25:07 -0500

    removes todo from dfa

--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -1,9 +1,9 @@
 function Prize({
-                 name,
-                 description,
-                 prize,
-                 className,
-               }: {
+  name,
+  description,
+  prize,
+  className,
+}: {
   name: string;
   description: string;
   prize: string;
@@ -14,11 +14,11 @@ function Prize({
       className={`border-2 border-gray-700 p-4 rounded-lg flex flex-col justify-between ${className}`}
     >
       <div>
-        <h3 className='text-lg text-blue'>{name}</h3>
+        <h3 className="text-lg text-blue">{name}</h3>
         <p>{description}</p>
       </div>
-      <div className=''>
-        <p className='text-sm mt-2 text-right'>{prize}</p>
+      <div className="">
+        <p className="text-sm mt-2 text-right">{prize}</p>
       </div>
     </div>
   );
@@ -26,62 +26,62 @@ function Prize({
 
 export default function About() {
   return (
-    <div className='py-16' id='prizes'>
-      <div className='flex'>
-        <div className='text-beige m-auto'>
-          <h3 className='text-2xl mb-4 text-center text-yellow'>Prizes</h3>
-          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-4 max-w-6xl'>
+    <div className="py-16" id="prizes">
+      <div className="flex">
+        <div className="text-beige m-auto">
+          <h3 className="text-2xl mb-4 text-center text-yellow">Prizes</h3>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-4 max-w-6xl">
             <Prize
-              name='Scott Krulcik Grand Prize'
-              description='Most impressive hack at TartanHacks 2023'
-              prize='$2000'
+              name="Scott Krulcik Grand Prize"
+              description="Most impressive hack at TartanHacks 2023"
+              prize="$2000"
             />
             <Prize
-              name='Most Disruptive: Technology'
-              description='Most significant technological innovation that disrupts the status quo.'
-              prize='4 × TBD'
+              name="Most Disruptive: Technology"
+              description="Most significant technological innovation that disrupts the status quo."
+              prize="4 × TBD"
             />
             <Prize
-              name='Most Disruptive: Product'
-              description='Most significant product/business model innovation that disrupts the status quo.'
-              prize='4 × TBD'
+              name="Most Disruptive: Product"
+              description="Most significant product/business model innovation that disrupts the status quo."
+              prize="4 × TBD"
             />
             <Prize
-              name='Best use of Algorand'
-              description='Best use of the Algorand protocol.'
-              prize='4 × TBD'
+              name="Best use of Algorand"
+              description="Best use of the Algorand protocol."
+              prize="4 × TBD"
             />
             <Prize
-              name='Sandia National Labs Award'
-              description='Decided by Sandia.'
-              prize='4 × Wireless Earphones'
+              name="Sandia National Labs Award"
+              description="Sponsored by Sandia."
+              prize="4 × Wireless Headphones"
             />
             <Prize
-              name='Best use of AI'
-              description='Best use of AI.'
-              prize='4 × Amazon Echo Dot'
+              name="Best use of AI"
+              description="Best use of AI."
+              prize="4 × Amazon Echo Dot"
             />
             <Prize
-              name='First Penguin Prize'
+              name="First Penguin Prize"
               description={`Inspired by Randy Pausch's The Last Lecture: “Experience is what you get when you don’t get what you wanted. And it can be the most valuable thing you have to offer.”
 Awarded to the team that took the biggest gamble while not meeting its goals… a prize for ‘glorious failure’.`}
-              prize='4 × TBD'
-              className='row-span-2'
+              prize="4 × TBD"
+              className="row-span-2"
             />
             <Prize
-              name='Best Design Hack'
-              description='Best product design.'
-              prize='4 × The Design of Everyday Things'
+              name="Best Design Hack"
+              description="Best product design."
+              prize="4 × The Design of Everyday Things"
             />
             <Prize
-              name='DFA Impact Prize'
-              description='TODO'
-              prize='Entry into DFA Impact, Tote Bags, Herb Kits'
+              name="DFA Impact Prize"
+              description=""
+              prize="Entry into DFA Impact, Tote Bags, Herb Kits"
             />
             <Prize
-              name='Spiciest Meme'
-              description='Spiciest meme in the #memes Discord channel.'
-              prize='What do you meme? card game'
+              name="Spiciest Meme"
+              description="Spiciest meme in the #memes Discord channel."
+              prize="What do you meme? card game"
             />
           </div>
         </div>
