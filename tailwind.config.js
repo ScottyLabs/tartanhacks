dcaf4675d04d0e966b2d7e93047add317c3dccac
pm3512 <aobolens@andrew.cmu.edu>
2023-12-31 20:58:58 -0500

    updated colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,21 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Futura', 'Trebuchet', 'MS', 'Arial', 'sans-serif'],
+    },
     extend: {
-      // TH 2023 colors
+      // TH 2024 colors
       colors: {
-        purple: '#4200ff',
-        blue: '#6e9afd',
-        yellow: '#fea801',
-        beige: '#f7f1e2',
+        purple: '#735fff',
+        yellow: '#f2dc00',
+        pink: '#ff7aa5',
+        blue: '#1028f1',
+        black: '#1B1818'
       },
+      fontFamily: {
+        title: ['Comba']
+      }
     },
   },
   plugins: [],
