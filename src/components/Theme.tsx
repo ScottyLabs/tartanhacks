caf90c20be03e827ced8c1facff0a339f0dbb6d7
Gabriel <adrastopoulos@gmail.com>
2024-01-02 14:04:58 -0800

    remove js from theme

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,11 +1,9 @@
 import Amplify from '../svg/Amplify';
 
 export default function Theme() {
-  const themeInfoString1 = `Make your ideas louder and heard across scales. It's about transforming whispers into roars, taking those sparks of creativity and turning them into blazing beacons of change. Whether it's a subtle improvement on an existing technology or a groundbreaking new concept, we encourage you to think audaciously and expansively.`;
-  const themeInfoString2 = `Think bigger and bolder, and let your small ideas amplify exponentially!`;
   return (
     <section
-      className="w-2/3 text-center text-white m-auto border-b-4 text-lg md:text-xl pt-36 -mb-36"
+      className="w-2/3 text-center text-white m-auto border-b-4 text-lg md:text-xl pt-36"
       id="theme"
     >
       <h1 className="mt-16 text-xl md:text-6xl font-bold">
@@ -14,8 +12,16 @@ export default function Theme() {
       <div className="m-auto my-96">
         <Amplify />
       </div>
-      <p className="max-w-2xl m-auto mb-6">{themeInfoString1}</p>
-      <p className="max-w-2xl m-auto mb-12">{themeInfoString2}</p>
+      <p className="max-w-2xl m-auto mb-6">
+        Make your ideas louder and heard across scales. It&apos;s about
+        transforming whispers into roars, taking those sparks of creativity and
+        turning them into blazing beacons of change. Whether it&apos;s a subtle
+        improvement on an existing technology or a groundbreaking new concept,
+        we encourage you to think audaciously and expansively.
+      </p>
+      <p className="max-w-2xl m-auto mb-12">
+        Think bigger and bolder, and let your small ideas amplify exponentially!
+      </p>
     </section>
   );
 }
