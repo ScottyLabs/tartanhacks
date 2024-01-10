d131dfbbe20bd70aaccf0115cf55d3d11bbb90f2
pm3512 <aobolens@andrew.cmu.edu>
2024-01-10 03:52:49 -0500

    added schedule section

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,66 +1,117 @@
-import styles from './Schedule.module.css';
-
-function TopBlock({
-  title,
-  time,
-  area,
-  location,
-}: {
-  title: string;
-  time: string;
-  area: string;
-  location?: string;
-}) {
-  return (
-    <div
-      className={`border p-1 rounded border-blue text-gray-200 flex flex-col sm:flex-row justify-center items-center`}
-      style={{ gridArea: area }}
-    >
-      <h1 className="text-lg text-center">{title}</h1>
-      <p className="text-sm text-center text-gray-400 sm:ml-4 flex flex-row">
-        <span>{time}</span>
-        {location && (
-          <>
-            <span className="mx-2">⸱</span>
-            <span>{location}</span>
-          </>
-        )}
-      </p>
-    </div>
-  );
-}
+import HackingBeginsCell from '../svg/HackingBegins';
+import HackingEndsCell from '../svg/HackingEnds';
+import ScheduleTitle from '../svg/Schedule';
 
 function Block({
   title,
   time,
-  area,
   location,
 }: {
   title: string;
   time: string;
-  area: string;
   location: string;
 }) {
   return (
     <div
-      className={`border p-2 rounded border-gray-600 flex flex-col justify-center`}
-      style={{ gridArea: area }}
+      className={`rounded flex flex-col md:flex-row w-full justify-between gap-2 md:gap-4`}
     >
-      <h1 className="text-lg text-center">{title}</h1>
-      <p className="text-sm text-center text-gray-400 flex flex-row justify-center">
-        <span>{time}</span>
-        <span className="mx-2 block">⸱</span>
-        <span>{location}</span>
-      </p>
+      <div className="border-white border-2 rounded-md py-8 px-4 w-full md:w-1/4 flex-col justify-center hidden md:flex">
+        <p className="text-center font-bold md:text-lg">{time}</p>
+      </div>
+      <div className="border-white border-2 rounded-md py-8 px-4 w-full md:w-3/4 flex flex-col">
+        <h1 className="text-center font-bold text-xl">{title}</h1>
+        <p className="text-center">{location}</p>
+        <p className="text-center md:hidden">{time}</p>
+      </div>
+    </div>
+  );
+}
+
+function Day({ date, children }: { date: string; children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col md:flex-row md:justify-between">
+      <h1 className='font-bold w-20 text-xl'>{date}</h1>
+      <div className="flex flex-col flex-1 align-middle gap-6">{children}</div>
     </div>
   );
 }
 
 export default function Schedule() {
   return (
-    <section className="py-16 text-white m-auto font-bold text-center border-b-4 w-2/3" id="schedule">
-    <h1 className='text-6xl mb-24'>Schedule</h1>
-    <p className='text-4xl font-title'>...Coming Soon...</p>
+    <section
+      className="pb-16 pt-48 -mb-36 border-b-4 w-2/3 m-auto flex flex-col gap-24"
+      id="schedule"
+    >
+      <ScheduleTitle className="m-auto w-full" />
+      <div className="text-white m-auto w-full">
+        <Day date="Feb 2.">
+          <Block
+            title="Check In Begins + Sponsorship Expo"
+            time="3:45 - 4:30 PM"
+            location="Connan"
+          />
+          <Block
+            title="Opening Ceremony"
+            time="4:30 - 5:30 PM"
+            location="McConomy (Please arrive by 4:00)"
+          />
+          <HackingBeginsCell className="-mt-[8%] -mb-[12%] z-10 w-[129%] -ml-[12%] relative" />
+          <Block
+            title="Team Formation Mixer"
+            time="5:30 - 6:30 PM"
+            location="McConomy"
+          />
+          <Block
+            title="Dinner"
+            time="6:30 - 7:30 PM"
+            location="Rangos"
+          />
+          <Block
+            title="Po-Shen Loh Talk"
+            time="7:30 - 8:30 PM"
+            location="Rangos"
+          />
+          <Block
+            title="Karaoke + Midnight Snack"
+            time="11:30 PM - 2:00 AM"
+            location="Connan"
+          />
+        </Day>
+        <div className='h-[1px] opacity-50 bg-white w-full my-4' />
+        <Day date="Feb 3.">
+          <Block
+            title="Breakfast"
+            time="9:30 - 10:30 AM"
+            location="Connan"
+          />
+          <Block
+            title="Yaser Sheikh Talk"
+            time="10:30 - 11:30 AM"
+            location="Connan"
+          />
+          <Block
+            title="Lunch"
+            time="1:00 - 2:00 PM"
+            location="Connan"
+          />
+          <HackingEndsCell className="-mt-[10%] -mb-[12%] z-10 w-[132%] -ml-[13%] relative" />
+          <Block
+            title="Judging Expo"
+            time="5:30 - 6:30 PM"
+            location="Connan"
+          />
+          <Block
+            title="Dinner"
+            time="6:30 - 7:30 PM"
+            location="PMW"
+          />
+          <Block
+            title="Closing Ceremony"
+            time="8:00 - 9:00 PM"
+            location="McConomy"
+          />
+        </Day>
+      </div>
     </section>
   );
 }
