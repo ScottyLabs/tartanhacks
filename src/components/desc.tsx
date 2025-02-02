d5cbe5f7a22ae6bac8784cedb3c658e95ccc1173
Anish Pallati <anishp@duck.com>
2025-02-02 15:34:14 -0500

    fix: remove typo in registration closed message

--- src/components/desc.tsx
+++ src/components/desc.tsx
@@ -12,7 +12,7 @@ export function Desc() {
           a 24-hour in person hackathon where participants from all over the
           country create innovative projects. Come on over to hack, connect with
           peers, and learn from our incredible speakers. Registration is now
-          closed for all sstudents who are not actively enrolled at CMU.
+          closed for all students who are not actively enrolled at CMU.
         </p>
       </div>
       <hr className="h-px my-8 w-3/4 md:w-2/3 bg-white border-0 z-30" />
