0ab6d8ce53f28d3d83a28334506c512d06f462a9
Xavier Lien <xavilien@gmail.com>
2025-02-02 18:58:35 -0500

    feat: add schedule

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -8,10 +8,128 @@ export function Schedule() {
       <h1 className="mt-24 px-10 uppercase font-basteleur text-5xl md:text-8xl z-30">
         Schedule
       </h1>
-      <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2 pt-8 z-30">
-        <p>
-          To be announced!
-        </p>
+      <div className="text-white text-center font-sfpro pt-8 z-30 mx-10 w-3/4 md:w-2/3">
+        <div className="grid grid-cols-[30px_1fr_2fr] md:grid-cols-[50px_1fr_2fr] gap-4">
+          <h2 className="text-md font-bold font-basteleur row-span-11">FRI</h2>
+          <div className="border-[1px] p-2 flex items-center justify-center row-span-2">4:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Check In Begins + Sponsorship Expo</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Team Formation Mixer</p>
+            <p className="">McConomy</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">4:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Opening Ceremony</p>
+            <p className="">McConomy (Please arrive by 4:00)</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center row-span-3">6:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10 bg-blue border-blue">
+            <img className="absolute size-14 -mt-3 -ml-9 md:size-36 md:-mt-10 md:-ml-7" src="../../public/hacking_swirl.png" alt="hackingSwirl" />
+            <p className="font-bold">Hacking Begins</p>
+            <p className="">McConomy</p>
+          </div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">AppLovin Talk</p>
+            <p className="">Danforth</p>
+          </div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Roboclub Workshop</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">7:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Story Protocol Talk</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">7:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Dinner</p>
+            <p className="">Rangos</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">8:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Ripple Talk</p>
+            <p className="">Danforth</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">8:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Telora Fellowship Talk</p>
+            <p className="">Danforth</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">9:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Oracle Talk</p>
+            <p className="">Danforth</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-white text-center font-sfpro pt-8 z-30 mx-10 w-3/4 md:w-2/3 mt-8">
+        <div className="grid grid-cols-[30px_1fr_2fr] md:grid-cols-[50px_1fr_2fr] gap-4">
+          <h2 className="text-md font-bold font-basteleur row-span-12">SAT</h2>
+          <div className="border-[1px] p-2 flex items-center justify-center">12:00 AM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Trivia + Midnight Snack</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">9:00 AM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">AppLovin Yoga</p>
+            <p className="">Activities Room</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">10:00 AM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Breakfast</p>
+            <p className="">Rangos</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center row-span-2">11:00 AM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">AppLovin Recruiting Event</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Quantum Computing Club Workshop</p>
+            <p className="">Danforth</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">12:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Zico Kolter Speaker Series</p>
+            <p className="">Danforth</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">1:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Lunch</p>
+            <p className="">Rangos</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center row-span-2">6:00 PM</div>
+          <div className="relative rounded-sm border-[1px] py-3 px-10 bg-blue border-blue">
+            <img className="absolute right-1 size-14 md:size-36 md:-mt-14 md:-ml-7 rotate-180" src="../../public/hacking_swirl.png" alt="hackingSwirl" />
+            <p className="font-bold">Hacking Ends</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Dinner + Performances</p>
+            <p className="">Rangos</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">7:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Judging Expo</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">8:30 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Merch + Boba</p>
+            <p className="">Connan</p>
+          </div>
+          <div className="border-[1px] p-2 flex items-center justify-center">9:00 PM</div>
+          <div className="rounded-sm border-[1px] py-3 px-10">
+            <p className="font-bold">Closing Ceremony</p>
+            <p className="">McConomy</p>
+          </div>
+        </div>
       </div>
     </section>
   );
