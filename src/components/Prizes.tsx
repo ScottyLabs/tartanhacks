c4f94be98bc11f35526aac9db8ff1fdd15a4f89d
Xavier Lien <xavilien@gmail.com>
2025-02-06 16:27:00 -0500

    feat: add scottylabs prizes

--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -8,9 +8,22 @@ export function Prizes() {
       <h1 className="mt-24 px-10 uppercase font-basteleur text-5xl md:text-8xl z-30">
         Prizes
       </h1>
-      <div className="text-white text-center text-xl font-sfpro w-2/3 md:w-1/2 pt-8 z-30">
-        <b className="font-bold">Grand Prize: $2,000</b>
-        <p>More prizes to be announced!</p>
+      <h1 className="mt-24 px-10 uppercase font-basteleur text-xl md:text-3xl z-30">
+        ScottyLabs
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 w-full items-center gap-4 z-30 px-10 pt-10 text-sfpro">
+        <div className="text-center p-4" style={{ backgroundImage: "url('/prize.svg')", backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+          <h2 className="font-bold text-lg">Scott Krulcik Grand Prize</h2>
+          <p className="text-lg font-basteleur">$5,000</p>
+        </div>
+        <div className="text-center p-4" style={{ backgroundImage: "url('/prize.svg')", backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+          <h2 className="font-bold text-lg">2nd Place Overall</h2>
+          <p className="text-lg font-basteleur">$2,000</p>
+        </div>
+        <div className="text-center p-4" style={{ backgroundImage: "url('/prize.svg')", backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+          <h2 className="font-bold text-lg">3rd Place Overall</h2>
+          <p className="text-lg font-basteleur">$1,000</p>
+        </div>
       </div>
     </section>
   );
